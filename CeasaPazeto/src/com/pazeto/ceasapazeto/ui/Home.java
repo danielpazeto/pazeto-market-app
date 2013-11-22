514a68418de61a51bc54d3df886e278d3f8250cf
Daniel <dpazetojr@gmail.com>
2013-11-22 19:44:35 -0200

    first commit

--- CeasaPazeto/src/com/pazeto/ceasapazeto/ui/Home.java
+++ CeasaPazeto/src/com/pazeto/ceasapazeto/ui/Home.java
@@ -1,48 +1,33 @@
 package com.pazeto.ceasapazeto.ui;
 
-import com.pazeto.ceasapazeto.R;
-import com.pazeto.ceasapazeto.db.DBFacade;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ListAdapter;
+
+import com.pazeto.ceasapazeto.R;
+import com.pazeto.ceasapazeto.db.DBFacade;
 
 public class Home extends Activity {
 
-	Button btVendas;
+	Button btCadProd;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
 		
 		DBFacade db = new DBFacade(getApplicationContext());
-		listaClientes();
-		//db.insertClientes();
 	
-	
-	}
-
-	
-	public void listaClientes() {
-		btVendas = (Button) findViewById(R.id.button1);
+		btCadProd = (Button) findViewById(R.id.ca);
 		
-		btVendas.setOnClickListener(new OnClickListener() {
+		btCadProd.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 
-				//DBFacade db = new DBFacade(getApplicationContext());
-				
 				Intent i = new Intent(Home.this, listaClientes.class);
 				startActivity(i);
 				
